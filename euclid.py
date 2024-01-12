06d8cc00b160167e01e83221a8c818d921bd1ce4
Kota <k.suzuki@4nonome.com>
2024-01-12 22:51:28 +0900

    レビューを踏まえた修正

--- euclid.py
+++ euclid.py
@@ -1,27 +1,17 @@
 import random                          # randomモジュールをimport
 import math
 
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
+a = int(input("a の値を入力: "))
+b = int(input("b の値を入力: "))
 
 # TODO
 
 # ----- 問3 -----
 def cal_gcm(a: int, b: int) -> int:    # 最大公約数(greatest common measure)を求める。
-    remainder = 1                      # 最初は、余り（remainder）を0ではない値に設定する。
-
     if a > b:                          # aがbよりも大きい時、aとbを入れ替える。
         a, b = b, a
-    
-    while not remainder == 0:          # 余りが0になるまで繰り返す。
-        quotient = int(b) // int(a)    # 商(quotient)を求める。
-        remainder = int(b) % int(a)    # 余りを求める。
-        if remainder == 0:             # 余りが0になった場合、whileループを抜ける。
-            break
-        else:                          # 余りが0でない場合は、次のループのためにaとbの値を更新する。
-            b = a
-            a = remainder
-    
+    while b != 0:
+        a, b = b, a % b
     return a
 
 gcm = cal_gcm(a, b)
--- prime_number.py
+++ prime_number.py
@@ -1,3 +1,5 @@
+import math
+
 a = int(input("aの値を入力: "))
 b = int(input("bの値を入力: "))
 
@@ -11,12 +13,13 @@ def is_prime_num(n: int) -> bool:
         return True
     if n == 3.14:                              # 1, 2行目でint型を指定しているため、ここは呼ばれないが、エクストラ問題の解答として記載。
         raise ValueError("整数を入力して下さい")
-    if n == -1:
+    if n < 0:
         raise ValueError("正の数を入力して下さい")
-    for i in range(2, n):
+    sqrt_n_int = math.floor(n**(1/2))          # 小数点以下を切り捨てたnの値（整数値）
+    for i in range(2, sqrt_n_int):
         if n % i == 0:                         # 割り切れると素数ではないことがわかる。
             return False
-        if i == (n - 1):                       # iが最後の値（n-1）になった場合、aは素数である。
+        if i == (sqrt_n_int - 1):              # iが最後の値（n-1）になった場合、aは素数である。
             return True
 
 is_prime_num_a = is_prime_num(a)               # 61の素数判定
